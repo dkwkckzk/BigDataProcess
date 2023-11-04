b0d65245221931458053507fa7d49dc324541be5
dkwkckzk <89624950+dkwkckzk@users.noreply.github.com>
2023-11-05 00:47:13 +0900

    HW2

---
+++ HW2/IMDBStudent20211743.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import sys
+
+# 커맨드라인 인자 체크
+if len(sys.argv) != 3:
+    print("주의!! IMDBStudent20151047.py <첫번째 인자:inputF> <두번째 인자:outputF>")
+    sys.exit()
+
+# 커맨드라인 인자에서 파일 이름 가져오기
+input_file = sys.argv[1] # movie.dat
+output_file = sys.argv[2] # movieoutput.txt
+
+# 장르별 영화 수를 저장할 사전 생성
+genre_dict = dict()
+
+# 입력 파일 movie.dat 파일 열기
+with open(input_file, 'r', encoding='utf-8') as f:
+    for line in f: # 파일 한 줄씩 정독
+        line = line.strip() # 양끝의 공백 제거
+        fields = line.split('::') # '::' 기준으로 쪼개기 → 3개 나오는 걸 확인
+        genres = fields[-1].split('|') # 마지막 필드(장르 정보)를 '|'로 쪼개기
+        for genre in genres: # 각 장르에 대하여
+            if genre in genre_dict: # 사전 안에 있다면
+                genre_dict[genre] += 1 # +1 해주기
+            else:
+                genre_dict[genre] = 1 # 없다면 넣기
+
+# 결과를 출력 movieoutput.txt로 저장
+with open(output_file, 'w', encoding='utf-8') as f:
+    for genre, count in genre_dict.items():
+        f.write("%s %d" % (genre, count))
---
+++ HW2/UBERStudent20211743.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+import sys
+from datetime import datetime
+
+# 커맨드라인 인자 체크
+if len(sys.argv) != 3:
+    print("주의!! IMDBStudent20151047.py <첫번째 인자:inputF> <두번째 인자:outputF>")
+    sys.exit()
+
+# 커맨드라인 인자에서 파일 이름 가져오기
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+# 데이터를 저장할 사전 생성
+uber_dict = dict()
+
+# 입력 파일 열기
+with open(input_file, 'r', encoding='utf-8') as f:
+    for line in f: # 파일 한 줄씩 정독
+        line = line.strip() # 양끝의 공백 제거
+        fields = line.split(',') # ',' 기준으로 쪼개기
+        region = fields[0] # 0: 지역
+        date_str = fields[1] # 1: 날짜
+        vehicles = int(fields[2]) # 2: 차량
+        trips = int(fields[3]) # 3: 운행된 수
+        
+        # 날짜 문자열을 datetime 객체로 변환
+        date = datetime.strptime(date_str, "%m/%d/%Y")
+        
+        # 요일 코드 생성
+        weekday = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
+        day = weekday[date.weekday()]
+        
+        # 지역과 요일을 키로 사용하여 데이터 저장
+        key = "%s,%s" % (region, day)
+        if key in uber_dict:
+            uber_dict[key][0] += vehicles
+            uber_dict[key][1] += trips
+        else:
+            uber_dict[key] = [vehicles, trips]
+
+# 결과를 출력 파일에 쓰기
+with open(output_file, 'w', encoding='utf-8') as f:
+    for key, value in uber_dict.items():
+       f.write("%s %d,%d\n" % (key, value[0], value[1]))
